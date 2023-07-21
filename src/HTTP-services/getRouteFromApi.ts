52fa473473bfbc84b64d2658c5135a9713e09ef9
AndreyFrekostya <orbita1872@gmail.com>
2023-07-21 18:31:19 +0700

    the problem with the marker icon has been solved

--- src/HTTP-services/getRouteFromApi.ts
+++ src/HTTP-services/getRouteFromApi.ts
@@ -2,5 +2,5 @@ export const getRouteFromApi=(point1:string, point2:string,point3:string)=>{
     const newPoint1=point1.split(',').reverse().join(',')
     const newPoint2=point2.split(',').reverse().join(',')
     const newPoint3=point3.split(',').reverse().join(',')
-    return fetch(`http://router.project-osrm.org/route/v1/driving/${newPoint1};${newPoint2};${newPoint3}?alternatives=true&geometries=polyline&overview=full&annotations=true` )
+    return fetch(`https://router.project-osrm.org/route/v1/driving/${newPoint1};${newPoint2};${newPoint3}?alternatives=true&geometries=polyline&overview=full&annotations=true` )
 }
--- src/components/MyMarker.tsx
+++ src/components/MyMarker.tsx
@@ -1,6 +1,16 @@
 import { LatLngTuple } from 'leaflet'
-import React,{FC} from 'react'
+import React,{FC, useEffect} from 'react'
 import { Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import iconMarker from "leaflet/dist/images/marker-icon.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: iconMarker,
+  shadowUrl: iconShadow,
+});
 interface IMyMarker{
     coords:LatLngTuple
 }
